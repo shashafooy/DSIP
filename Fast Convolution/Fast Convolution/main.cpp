c22120d17079491c5113e5aca4dc456d0feb8374
Aaron <aaron.pettit@comcast.net>
2018-12-05 17:41:12 -0700

    Finished Fast Convolution. Added different configuration to switch between filter and fft. Started Report

--- Fast Convolution/Fast Convolution/main.cpp
+++ Fast Convolution/Fast Convolution/main.cpp
@@ -1,11 +1,9 @@
-#include "fileRead.h"
 #include "Convolution.h"
 #include "Timer.h"
 
 
 int main(const int argc, char *argv[])
 {
-	//Print out the impuse response
 	if (argc < 3)
 	{
 		printf("Wrong number of arguments");
@@ -14,10 +12,19 @@ int main(const int argc, char *argv[])
 
 	Convolution filter(argv[1], argv[2]);
 
+#ifdef _FFT
+	Timer::Start();
+	filter.FastConvolution();
+	Timer::Stop();
+	printf("Fast Convolution using overlap-save duration: %0.3f seconds\n", Timer::GetDuration());
+#else
 	Timer::Start();
 	filter.Filter();
 	Timer::Stop();
-	printf("Polyphase duration: %f", Timer::GetDuration());
+	printf("Filter duration: %0.3f seconds\n", Timer::GetDuration());
+#endif
 
+	//system("pause");
+	return 0;
 }
 
